5d848b1194d399952581a80d8b12a4b678b5181f
Nibedita Sahu <126712682+nibeditans@users.noreply.github.com>
2023-05-09 15:10:11 +0530

    Update HEALTH_MANAGEMENT_SYSTEM.py

--- HEALTH_MANAGEMENT_SYSTEM.py
+++ HEALTH_MANAGEMENT_SYSTEM.py
@@ -1,5 +1,5 @@
 # Health Management System
-# There are 3 clients - Chicku, Picku and Jani
+# There are 3 clients - Nibe, Dita and Nibedita
 
 
 # Total 6 files
@@ -15,85 +15,85 @@ def gettime():
 
 def take(n):
     if n == 1:
-        j = int(input("Enter 1 for excersise & 2 for food: "))
-        if j == 1:
+        i = int(input("Enter 1 for excersise & 2 for food: "))
+        if i == 1:
             value = input("Type here\n")
-            with open("Chicku-ex.txt", "a") as cp:
-                cp.write(str([str(gettime())]) + ": " + value + "\n")
+            with open("Nibe-ex.txt", "a") as ns:
+                ns.write(str([str(gettime())]) + ": " + value + "\n")
             print("Successfully written")
-        elif j == 2:
+        elif i == 2:
             value = input("Type here\n")
-            with open("Chicku-food.txt", "a") as cp:
-                cp.write(str([str(gettime())]) + ": " + value + "\n")
+            with open("Nibe-food.txt", "a") as ns:
+                ns.write(str([str(gettime())]) + ": " + value + "\n")
             print("Successfully written")
     elif n == 2:
-        j = int(input("Enter 1 for excersise & 2 for food: "))
-        if j == 1:
+        i = int(input("Enter 1 for excersise & 2 for food: "))
+        if i == 1:
             value = input("Type here\n")
-            with open("Picku-ex.txt", "a") as cp:
-                cp.write(str([str(gettime())]) + ": " + value + "\n")
+            with open("Dita-ex.txt", "a") as ns:
+                ns.write(str([str(gettime())]) + ": " + value + "\n")
             print("Successfully written")
-        elif j == 2:
+        elif i == 2:
             value = input("Type here\n")
-            with open("Picku-food.txt", "a") as cp:
-                cp.write(str([str(gettime())]) + ": " + value + "\n")
+            with open("Dita-food.txt", "a") as ns:
+                ns.write(str([str(gettime())]) + ": " + value + "\n")
             print("Successfully written")
     elif n == 3:
-        j = int(input("Enter 1 for excersise & 2 for food: "))
-        if j == 1:
+        i = int(input("Enter 1 for excersise & 2 for food: "))
+        if i == 1:
             value = input("Type here\n")
-            with open("Jani-ex.txt", "a") as cp:
-                cp.write(str([str(gettime())]) + ": " + value + "\n")
+            with open("Nibedita-ex.txt", "a") as ns:
+                ns.write(str([str(gettime())]) + ": " + value + "\n")
             print("Successfully written")
-        elif j == 2:
+        elif i == 2:
             value = input("Type here\n")
-            with open("Jani-food.txt", "a") as cp:
-                cp.write(str([str(gettime())]) + ": " + value + "\n")
+            with open("Nibedita-food.txt", "a") as ns:
+                ns.write(str([str(gettime())]) + ": " + value + "\n")
             print("Successfully written")
     else:
-        print("Please enter valid input (1(Chicku),2(Picku),3(Jani)")
+        print("Please enter valid input (1(Nibe),2(Dita),3(Nibedita)")
 
 def retrieve(n):
     if n == 1:
-        j = int(input("Enter 1 for excersise & 2 for food: "))
-        if j == 1:
-            with open("Chicku-ex.txt") as cp:
-                for i in cp:
-                    print(i, end="")
-        elif j == 2:
-            with open("Chicku-food.txt") as cp:
-                for i in cp:
-                    print(i, end="")
+        i = int(input("Enter 1 for excersise & 2 for food: "))
+        if i== 1:
+            with open("Nibe-ex.txt") as ns:
+                for k in ns:
+                    print(k, end="")
+        elif i == 2:
+            with open("Nibe-food.txt") as ns:
+                for k in ns:
+                    print(k, end="")
     elif n == 2:
-        j = int(input("Enter 1 for excersise & 2 for food: "))
-        if j == 1:
-            with open("Picku-ex.txt") as cp:
-                for i in cp:
-                    print(i, end="")
-        elif j == 2:
-            with open("Picku-food.txt") as cp:
-                for i in cp:
-                    print(i, end="")
+        i = int(input("Enter 1 for excersise & 2 for food: "))
+        if i == 1:
+            with open("Dita-ex.txt") as ns:
+                for k in ns:
+                    print(k, end="")
+        elif i == 2:
+            with open("Dita-food.txt") as ns:
+                for k in ns:
+                    print(k, end="")
     elif n == 3:
-        j = int(input("Enter 1 for excersise & 2 for food: "))
-        if j == 1:
-            with open("Jani-ex.txt") as cp:
-                for i in cp:
-                    print(i, end="")
-        elif j == 2:
-            with open("Jani-food.txt") as cp:
-                for i in cp:
-                    print(i, end="")
+        i = int(input("Enter 1 for excersise & 2 for food: "))
+        if i == 1:
+            with open("Nibedita-ex.txt") as ns:
+                for k in ns:
+                    print(k, end="")
+        elif i == 2:
+            with open("Nibedita-food.txt") as ns:
+                for k in ns:
+                    print(k, end="")
     else:
-        print("Please enter valid input (Chicku, Picku and Jani)")
+        print("Please enter valid input (Nibe, Dita and Nibedita)")
 
 
 print("Health Management System! ")
 a = int(input("Press 1 for log the value & 2 for retrieve the value: "))
 
 if a == 1:
-    b = int(input("Press 1 for Chicku, 2 for Picku & 3 for Jani: "))
+    b = int(input("Press 1 for Nibe, 2 for Dita & 3 for Nibedita: "))
     take(b)
 else:
-    b = int(input("Press 1 for Chicku, 2 for Picku & 3 for Jani: "))
+    b = int(input("Press 1 for Nibe, 2 for Dita & 3 for Nibedita: "))
     retrieve(b)
